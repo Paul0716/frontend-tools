451591510c3afbfc2cb06fb65c70897f8f362571
Paul Ku <Paul.Ku@redcypress.com.tw>
2019-04-23 18:27:56 +0800

    order import

---
+++ tslint.js
@@ -0,0 +1,11 @@
+module.exports = {
+    rules: {
+        "ordered-imports": [
+            true,
+            {
+                "import-sources-order": "lowercase-last",
+                "named-imports-order": "lowercase-first"
+            }
+        ]
+    }
+}
